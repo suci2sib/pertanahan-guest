d2bbb4cb65ff200947f60da7a799daa71790dda3
arsyad0098 <arsyad24si@mahasiswa.pcr.ac.id>
2025-11-11 11:05:13 +0700

    data base seeder

---
+++ database/seeders/CreateWargaDummy.php
@@ -0,0 +1,30 @@
+<?php
+namespace Database\Seeders;
+
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CreateWargaDummy extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('warga')->insert([
+                'no_ktp'        => $faker->numerify('################'), // 16 digit angka acak
+                'nama'          => $faker->name(),
+                'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+                'agama'         => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Budha', 'Konghucu']),
+                'pekerjaan'     => $faker->jobTitle(),
+                'telp'          => $faker->phoneNumber(),
+                'email'         => $faker->unique()->safeEmail(),
+            ]);
+        }
+    }
+
+}
